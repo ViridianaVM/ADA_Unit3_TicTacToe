c0fe519a4f7582275da4c51f3e6eb7feaf62432e
Rock - Viri Vazquez <viridiana.vzq@gmail.com>
2021-06-22 19:11:55 -0700

    generateSquareComponents function implemented to convert a 2D array to 1D array of 9 square components

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,16 +5,39 @@ import PropTypes from 'prop-types';
 
 
 const generateSquareComponents = (squares, onClickCallback) => {
+  
+  console.log('My squares inputs: ', squares);
+  console.log('My onClickCallBack inputs: ', onClickCallback);
+
+  let squaresOneDimension = [];
+  for (let idx=0; idx<squares.length; idx++){
+    squaresOneDimension = squaresOneDimension.concat(squares[idx]);
+  }
+
+  const squareComponents = squaresOneDimension.map((square) => {
+    return (
+      <Square key={square.id}
+      id={square.id}
+      value={square.value}
+      onClickCallback={square.onClickCallback}
+      />
+    );
+  });
+
+  return squareComponents;
+}
+
+
   // Complete this for Wave 1
   // squares is a 2D Array, but 
   //  you need to return a 1D array
   //  of square components
 
-}
+
 
 const Board = ({ squares, onClickCallback }) => {
   const squareList = generateSquareComponents(squares, onClickCallback);
-  console.log(squareList);
+  console.log('This is the square list',squareList);
   return <div className="grid" >
     {squareList}
   </div>
